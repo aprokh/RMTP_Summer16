2fcc6f8758c9909b5a348e5f78a647cc4f399e56
Andrew Nitz <acn23@pitt.edu>
2016-11-13 14:19:19 +0400

    js update

--- obdurodonRmtpWebPage/javascript/javascriptFilename.js
+++ obdurodonRmtpWebPage/javascript/javascriptFilename.js
@@ -3,15 +3,18 @@ window.onload = function(){
     var togglePath = location.pathname
     var togglePath = togglePath.split('.')[0]
     
-    if (togglePath.indexOf('Russ') != -1){
-        togglePath = togglePath.split('Russ')[0].concat('.xhtml')        
+    if (togglePath.indexOf('.edu/shtml') > -1){
+        togglePath = togglePath.concat('indexRuss.shtml')
+    }
+    else if (togglePath.indexOf('Russ') != -1){
+        togglePath = togglePath.split('Russ')[0].concat('.shtml')        
     }
     else {
-        togglePath = togglePath.concat('Russ.xhtml')
+        togglePath = togglePath.concat('Russ.shtml')
     }
     
     //Deal with index page by checking for unique situation, and dealing with.
-    if (togglePath.indexOf('/.xhtml') != -1){
+    if (togglePath.indexOf('/.shtml') != -1){
        togglePath = togglePath.substring(0, togglePath.lastIndexOf('/') + 1);
     }
     var langToggle = document.getElementById('langToggle');
